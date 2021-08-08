210ebcfe224252612319b97a8c7ec230dd293f21
Pavel Janovsky <janovsky.paja@gmail.com>
2021-08-08 18:29:29 +0000

    Add hello world.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,7 +29,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
+      //  SpringApplication.run(PetClinicApplication.class, args);
+      System.out.println("Hello World");
     }
 
 }
